242af6b84e58123b6bfb1e1a088b9862be890b6d
akshaychavhan <akshay@rtledgers.com>
2023-11-04 15:44:27 +0530

    Recover password screen working

--- server/config.js
+++ server/config.js
@@ -1,3 +1,5 @@
 export default {
-    JWT_SECRET : "v6BFBHItSY3Hn+MMmWhHrHdLOyJeODc5mY0iMvp1MQ4="
+    JWT_SECRET : "v6BFBHItSY3Hn+MMmWhHrHdLOyJeODc5mY0iMvp1MQ4=",
+    EMAIL : "erling.dibbert@ethereal.email",
+    PASSWORD : "tA6aqCVDGx6x2hn6yc"
 }
